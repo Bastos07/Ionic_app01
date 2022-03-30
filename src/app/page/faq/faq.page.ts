84af085c5d75e67c0ed55fbf2e3896c2464057ed
Luferat <33375030+Luferat@users.noreply.github.com>
2022-03-30 15:29:21 -0300

    Primeiros testes com FAQ

--- src/app/page/faq/faq.page.ts
+++ src/app/page/faq/faq.page.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 
 // Importa todas as dependências necessárias
-import { initializeApp } from 'firebase/app';
-import { collection, query, where, onSnapshot, getFirestore } from 'firebase/firestore';
-import { environment } from 'src/environments/environment';
+import { collection, collectionChanges, collectionData, CollectionReference, Firestore, query } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-faq',
@@ -13,46 +11,22 @@ import { environment } from 'src/environments/environment';
 })
 export class FaqPage implements OnInit {
 
-  ///// Variáveis da página /////
-
-  // Conexão com o Firebase
-  app = initializeApp(environment.firebase);
-
-  // Conexão com o banco de dados
-  db = getFirestore();
-
   // Vai armazenar cada FAQ obtido
   faq: any;
 
   // Vai armazenar todos os FAQ obtidos para a view
-  faqs: Array<any> = [];
+  faqs: any;
 
-  constructor(private alertController: AlertController) { }
+  constructor(
+    private afs: Firestore,
+    private alertController: AlertController
+  ) { }
 
   ngOnInit() {
 
-    // Conectar ao banco de dados e obtém todos os documentos da coleção 'faq'
-    onSnapshot(query(collection(this.db, 'faq')), (myFaqs) => {
-
-      // Limpa a lista de FAQ para carregar novamente.
-      this.faq = [];
-
-      // Loop que itera cada faq obtida
-      myFaqs.forEach((doc) => {
-
-        // Armazena dados na variável 'faq'
-        this.faq = doc.data();
-
-        // Armazena o ID do documento em 'faq'
-        this.faq.id = doc.id;
-
-        // Adiciona conteúdo de 'faq' em 'faqs' para ser usado na view
-        this.faqs.push(this.faq);
-
-      });
-
+    collectionData(collection(this.afs, 'faq'), { idField: 'id' }).subscribe((faqs) => {
+      this.faqs = faqs;
     });
-
   }
 
   async show(alertTitle, alertText) {
